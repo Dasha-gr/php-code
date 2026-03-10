9537dc4959e4ca7a479935d4b1aca53e2a301908
Dasha Gromova <dgromova560@gmail.com>
2026-03-10 16:41:02 +0300

    Изменение в файле index.php

--- index.php
+++ index.php
@@ -44,36 +44,36 @@
 //if (($b > $a) and ($a > $c) and ($b< $c))
 //    echo $a." ".$b." ".$c. "<br>";
 //elseif (($a < $с) and ($b< $c))
-
-/**
- *
- * Функции
- *
- */
+//
+///**
+// *
+// * Функции
+// *
+// */
 // Задача 1
-function kv_ch($a)
-{
-    return $a*$a;
-}
-echo "<br>";
-print ("Число в квадрате:");
-print (kv_ch(3));
-
-// Задача 2
-function max_ch($a=6, $b=2)
-{
-    if ($a > $b){
-        echo "Число а больше числа б";
-    }
-    else{
-        echo "Число б больше числа а";
-    }
-}
-echo "<br>";
-max_ch();
-
-// Задача 3
-$arr_1 = array(1,2,3,4,5,6,7,8,33);
+//function kv_ch($a)
+//{
+//    return $a*$a;
+//}
+//echo "<br>";
+//print ("Число в квадрате:");
+//print (kv_ch(3));
+//
+//// Задача 2
+//function max_ch($a=6, $b=2)
+//{
+//    if ($a > $b){
+//        echo "Число а больше числа б";
+//    }
+//    else{
+//        echo "Число б больше числа а";
+//    }
+//}
+//echo "<br>";
+//max_ch();
+//
+//// Задача 3
+//$arr_1 = array(1,2,3,4,5,6,7,8,33);
 
 //function max_el($arr_1)
 //{
@@ -83,27 +83,27 @@ $arr_1 = array(1,2,3,4,5,6,7,8,33);
 //print ("Максимальный элемент:");
 //print(max_el($arr_1));
 
-function max_el($arr_1)
-{
-    if (isset($arr_1)){
-        $max_i = 0;
-        for($i=0;$i<count($arr_1);$i++){
-
-        }
-    }
-}
+//function max_el($arr_1)
+//{
+//    if (isset($arr_1)){
+//        $max_i = 0;
+//        for($i=0;$i<count($arr_1);$i++){
+//
+//        }
+//    }
+//}
 
 // Задача 4
-function pl_kv ($a)
-{
-    return $a*$a;
-}
-echo "<br>";
-print("Площадь квадрата:");
-print (pl_kv(4));
+//function pl_kv ($a)
+//{
+//    return $a*$a;
+//}
+//echo "<br>";
+//print("Площадь квадрата:");
+//print (pl_kv(4));
 
 // Задача 5
-$arr = array('a1' => 1, 'a2' => 2, 'a3' => 3);
+//$arr = array('a1' => 1, 'a2' => 2, 'a3' => 3);
 //function dlina_mas($arr)
 //{
 //    return count($arr);
@@ -112,8 +112,146 @@ $arr = array('a1' => 1, 'a2' => 2, 'a3' => 3);
 //print ("Длина ассоциативного массива:");
 //print (dlina_mas($arr));
 
-function dl_mas($arr)
-{
-    if ()
+//class Test{
+//    public $a=4;
+//    public $b=2;
+//
+//    public function sub() {
+//        return $this->a - $this->b;
+//    }
+//
+//    public function mul() {
+//        return $this->a * $this->b;
+//    }
+//}
+//
+//$obj = new Test();
+//print $obj->sub();
+//echo "<br>";
+//print $obj->mul();
+
+// Класс со свойствами : ФИО,ВОЗРАСТ, ВЕС, РОСТ, МЕТОД ВЫЧЕСТЬ ИНДЕКС МАССЫ ТЕЛА
+
+//class Person{
+//    public $fam;
+//    public $name;
+//    public $ote;
+//    public $age;
+//    public $weight;
+//    public $height;
+//
+//    public $ind_mas;
+//
+////    function __destruct(){
+////        print "destruct";
+////    } // Функция __destruct
+//
+//
+//    function __construct($fam,$name,$ote,$age,$weight,$height){
+//        $this->fam=$fam;
+//        $this->name=$name;
+//        $this->ote=$ote;
+//        $this->age=$age;
+//        $this->weight=$weight;
+//        $this->height=$height;
+//    } // Функция __construct
+//
+//    function index_mas(){
+//        $h = $this->height/100;
+//        $this->ind_mas= $this->weight/($h*$h);
+//        print $this->ind_mas;
+//    }
+//
+//
+//} // Класс Person
+//
+//$obj = new Person("Иванов", "Иван", "Иванович", 23, 78, 179);
+//print "Фамилия: ".$obj->fam."<br>";
+//print "Имя: ".$obj->name."<br>";
+//print "Отчество: ".$obj->ote."<br>";
+//print "Возраст: ".$obj->age."<br>";
+//print "Вес: ".$obj->weight."<br>";
+//print "Рост: ".$obj->height."<br>";
+//print "ИМТ:" .$obj->ind_mas."<br>";
+
+//class Circle {
+//    public $r;
+//    const pi=3.14;
+//
+//    function __construct($r) {
+//        $this->r = $r;
+//    }
+//
+//    function pl_circle() {
+//        return self::pi*($this->r * $this->r);
+//    }
+//}
+//$obj = new Circle(4);
+//print_r("Площадь круга:".$obj->pl_circle());
+
+
+class People{
+    public $name; // Имя
+    public $town; // Поселение
+    public $xp; // Хп
+    public $damage; //Урон
+
+
+    function __construct($name, $town, $damage)
+    {
+        $this->name = $name;
+        $this->town = $town;
+        $this->xp = 100;
+        $this->damage = rand(0,20);
+    }
+
+    function damage_voin($xp, $damage) //Урон воина
+    {
+
+    }
 }
+
+
+class Zhitel extends People{
+    function __construct($name, $town)
+    {
+        $this->name = $name;
+        $this->town = $town;
+        $this->xp = 100;
+    }
+} //Zhitel
+
+class Voin extends People{
+
+    function __construct($name, $town)
+    {
+        $this->name = $name;
+        $this->town = $town;
+        $this->xp = 100;
+    }
+
+} //Voin
+
+class Doctor extends People{
+    public $hilks; //Хилка, лечение
+
+    function __construct($name, $town, $hilks)
+    {
+        $this->name = $name;
+        $this->town = $town;
+        $this->xp = 100;
+        $this->hilks = $hilks;
+    }
+
+} //Doctor
+
+$zh_1 = new Zhitel("Боб", "Суманара");
+$zh_2 = new Zhitel("Тод", "Анара")
+
+
+
+
 ?>
+
+
+
